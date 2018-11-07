492ee9167fecbb66dc75705b27db935a250ffe3b
Tomoka <Tomoka@Tomokas-MacBook-Pro.local>
2018-11-06 22:17:26 -0500

    created C++ file

---
+++ Algorithms.cpp
@@ -0,0 +1,9 @@
+//
+//  Algo1.cpp
+//  
+//
+//  Created by Tomoka Kan on 11/6/18.
+//
+//
+
+#include "Algorithms.hpp"
---
+++ Algorithms.hpp
@@ -0,0 +1,14 @@
+//
+//  Algo1.hpp
+//  
+//
+//  Created by Tomoka Kan on 11/6/18.
+//
+//
+
+#ifndef Algo1_hpp
+#define Algo1_hpp
+
+#include <stdio.h>
+
+#endif /* Algo1_hpp */
